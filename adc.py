d5b296b11271281ddaa82c63d1cd1f7260931659
Ryan Plante <rplante97@gmail.com>
2019-03-03 12:35:08 -0500

    SQL insert done

--- adc.py
+++ adc.py
@@ -7,6 +7,25 @@ from w1thermsensor import W1ThermSensor
 from dataProcess import *
 from motor import *
 from camera import *
+import MySQLdb
+
+
+def database_insert(datestring, temp, humidity, soil, soil2, light):
+        db = MySQLdb.connect(
+                host = "localhost",
+                user="pi",
+                passwd="password",
+                db='environment_data'
+                )
+        cursor = db.cursor()
+        mysqlStatement = "INSERT INTO sensor_data (ID, time, temp, humidity, soil_moisture, soil_moisture2, light_level) VALUES (NULL, '%s', %f, %f, %i, %i ,%i)" % \
+                         (datestring, temp, humidity, soil, soil2, light)
+
+        cursor.execute(mysqlStatement)
+        db.commit();
+        db.close();
+
+
 
 # Hardware SPI configuration:
 SPI_PORT   = 0
@@ -18,24 +37,24 @@ s = W1ThermSensor()
 
 try:
 	# Initialize the motor
-	initMotor()
+	#initMotor()
 
-	turnMotor(1, 500) # 500 steps CCW
-	turnMotor(1, -500) # 500 steps CW
-	turnMotor(2, 500) # 500 steps CCW
-	turnMotor(2, -500) # 500 steps CW
+	#turnMotor(1, 500) # 500 steps CCW
+	#turnMotor(1, -500) # 500 steps CW
+	#turnMotor(2, 500) # 500 steps CCW
+	#turnMotor(2, -500) # 500 steps CW
 
 	# Code to read ADC
 	# var = mcp.read_adc(channel_num)
 
 	# Do 5 dummy reads originally to initialize ADC
-	x = mcp.read_adc(0)
-	x = mcp.read_adc(1)
-	x = mcp.read_adc(2)
-	x = mcp.read_adc(3)
-	x = mcp.read_adc(4)
-
+	
 	while True:
+                x = mcp.read_adc(0)
+                x = mcp.read_adc(1)
+                x = mcp.read_adc(2)
+                x = mcp.read_adc(3)
+                x = mcp.read_adc(4)
 
 		# Read light status (int)	
 		light = adc2light(mcp.read_adc(0))
@@ -48,14 +67,16 @@ try:
 
 		# Read soil moisture (integer percent)
 		soil = adc2soilmoist(mcp.read_adc(3))
+		soil2 = adc2soilmoist(mcp.read_adc(4))
 
 		# Get current time
 		curr = datetime.datetime.now()
 		datestring = str(curr)
 
 		print datestring, light, " ", humid, " ", soil, temp
-
-		time.sleep(2)
+                database_insert(datestring, temp, humid, soil, soil2, light)
+		
+		time.sleep(30)
 
 except KeyboardInterrupt:
 	deinitMotor()
